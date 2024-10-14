8bfcbe09137db559f434b0c095864f3b9b6ecb45
Bekzat Baubek <39556077+bekzatbaubek@users.noreply.github.com>
2024-10-14 11:32:56 +0800

    chore: simple client setup

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,4 +1,27 @@
+const baseUrl = "http://localhost:8080";
+
 console.info("Loaded Hurdle");
+
+interface Guess {
+  round: number;
+  guess: string;
+  hint: string;
+}
+
+interface GameSession {
+  current_round: number;
+  guesses: Guess[];
+}
+
+async function createOrLoadSession() {
+  let response = await fetch(`${baseUrl}/api/sessions`, {
+    credentials: "include",
+  });
+  let session: GameSession = await response.json();
+  console.log("Session:", session);
+}
+
+createOrLoadSession();
 
 let hurdleForm = document.querySelector("#hurdle-form");
 
@@ -13,5 +36,15 @@ if (!hurdleForm) {
 
     let guess = formData.get("hurdle-guess") as string;
     console.log("Guess:", guess);
+
+    fetch(`${baseUrl}/api/guesses`, {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify({ guess: guess }),
+    }).then((response) => {
+      response.json().then((data) => {
+        console.log("Response data:", data);
+      });
+    });
   });
 }
